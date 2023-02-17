a6db7477bcc7dd08858d7cc0552a6eec093b93fc
CORP\DLORUSSO <domenico.lorusso@capgemini.com>
2023-02-17 17:18:20 +0100

    basket.Api Rewrited

---
+++ src/Services/Basket1/Basket.API1/Repositories/IBasketRepository.cs
@@ -0,0 +1,11 @@
+﻿using Basket.API1.Entities;
+
+namespace Basket.API1.Repositories
+{
+    public interface IBasketRepository
+    {
+        Task<ShoppingCart> GetBasket(string userName);
+        Task<ShoppingCart> UpdateBasket(ShoppingCart basket);
+        Task DeleteBasket(string userName);
+    }
+}
