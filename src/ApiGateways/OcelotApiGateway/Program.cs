11c3640a41195286ca441968aa7bb3145173aee1
CORP\DLORUSSO <domenico.lorusso@capgemini.com>
2023-04-03 17:07:43 +0200

    contenairazed ocelot ok

--- src/ApiGateways/OcelotApiGateway/Program.cs
+++ src/ApiGateways/OcelotApiGateway/Program.cs
@@ -1,8 +1,19 @@
+using Microsoft.Extensions.Hosting.Internal;
 using Microsoft.Extensions.Logging;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using Ocelot.Cache.CacheManager;
+using Microsoft.AspNetCore.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.ConfigureAppConfiguration(hostingContext =>
+    {
+        builder.Configuration.AddJsonFile($"ocelot." +
+            $"{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}" +
+            $".json", true,true);
+
+    });
 builder.Host.ConfigureLogging(loggingbuilder =>
 {
     loggingbuilder.AddConfiguration(builder.Configuration.GetSection("Logging"));
@@ -10,13 +21,20 @@ builder.Host.ConfigureLogging(loggingbuilder =>
     loggingbuilder.AddDebug();
 });
 
-
-builder.Services.AddOcelot();
+builder.Services.AddOcelot().AddCacheManager(settings => settings.WithDictionaryHandle());
 
 var app = builder.Build();
 
+app.UseRouting();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapGet("/", async context =>
+    {
+        await context.Response.WriteAsync("Hello World!");
+    });
+});
 await app.UseOcelot();
 
-app.MapGet("/", () => "Hello World!");
 
 app.Run();
